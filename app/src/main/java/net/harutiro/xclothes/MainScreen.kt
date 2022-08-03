32647426953a984133e4a0c781766f9a613f34e7
harutiro <harutiro2027@outlook.jp>
2022-08-03 20:18:05 +0900

    add:Fabの作成

--- app/src/main/java/net/harutiro/xclothes/MainScreen.kt
+++ app/src/main/java/net/harutiro/xclothes/MainScreen.kt
@@ -1,14 +1,19 @@
 package net.harutiro.xclothes
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.padding
+import android.content.Intent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -18,7 +23,9 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import net.harutiro.test_bottomnavigation_withjetpackcompose.BottomBarScreen
 import net.harutiro.test_bottomnavigation_withjetpackcompose.BottomNavGraph
+import net.harutiro.xclothes.ui.theme.XclothesTheme
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
@@ -27,8 +34,19 @@ fun MainScreen() {
 
     Scaffold (
         topBar = { TopBar()},
-        bottomBar = {BottomBar(navController = navController) }
+        bottomBar = { BottomBar(navController = navController) } ,
+//        floatingActionButton = {
+//            FloatingActionButton(
+//                onClick = {
+//
+//                },
+//            ) {
+//                Icon(Icons.Filled.Add, contentDescription = "追加")
+//            }
+//        },
+//        floatingActionButtonPosition = FabPosition.Center,
     ){
+
         BottomNavGraph(navController = navController)
     }
 }
@@ -56,7 +74,13 @@ fun BottomBar(navController: NavHostController){
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
 
-    NavigationBar{
+    BottomAppBar(
+        modifier = Modifier
+            .height(80.dp)
+            .clip(RoundedCornerShape(15.dp, 15.dp, 0.dp, 0.dp)),
+//        cutoutShape = CircleShape,
+        tonalElevation = 22.dp
+    ){
         screens.forEach { screen ->
             AddItem(
                 screen = screen,
@@ -96,4 +120,12 @@ fun RowScope.AddItem(
             }
         }
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    XclothesTheme {
+        MainScreen()
+    }
 }
--- app/src/main/java/net/harutiro/xclothes/screens/Home.kt
+++ app/src/main/java/net/harutiro/xclothes/screens/Home.kt
@@ -1,9 +1,13 @@
 package net.harutiro.test_bottomnavigation_withjetpackcompose.screens
 
+import android.content.Intent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
@@ -11,15 +15,44 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import net.harutiro.xclothes.CoordinateEditActivity
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Magenta),
-        contentAlignment = Alignment.Center
-    ) {
+
+    val mContext = LocalContext.current
+
+    Scaffold(
+        modifier = Modifier.padding(0.dp,0.dp,0.dp,70.dp),
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { mContext.startActivity(Intent(mContext, CoordinateEditActivity::class.java))},
+            ) {
+                Icon(Icons.Filled.Add, contentDescription = "追加")
+            }
+        }
+
+    ){
+
+        Box(
+
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Magenta)
+                .padding(it),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "HOME",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        }
+
         Text(
             text = "HOME",
             fontSize = 32.sp,
@@ -27,6 +60,9 @@ fun HomeScreen() {
             color = Color.White
         )
     }
+
+
+
 }
 
 @Composable

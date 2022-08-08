6df8f3a634d6e9ff11a03b78f73958813fa272dd
harutiro <harutiro2027@outlook.jp>
2022-08-09 04:21:42 +0900

    update:写真がきれいに映るようになった

--- app/src/main/java/net/harutiro/xclothes/screens/add/Add.kt
+++ app/src/main/java/net/harutiro/xclothes/screens/add/Add.kt
@@ -1,22 +1,28 @@
 package net.harutiro.test_bottomnavigation_withjetpackcompose.screens
 
 import android.content.res.Configuration
+import android.net.Uri
+import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
-import net.harutiro.xclothes.screens.ProfileScreen
+import net.harutiro.xclothes.R
 import net.harutiro.xclothes.screens.add.AddViewModel
 import net.harutiro.xclothes.ui.theme.XclothesTheme
 
@@ -33,11 +39,43 @@ fun AddScreen(viewModel: AddViewModel) {
 
     Surface (
         modifier = Modifier
-            ){
-        Button(onClick = { launcher.launch(uri) }) {
+    ){
+        Column {
+            if(uriRemember.isEmpty()){
+                nottingPhotoIcon(launcher,uri)
+            }else{
+                photoView(uriRemember)
+            }
+
 
         }
+    }
+}
 
+@Composable
+fun nottingPhotoIcon(launcher: ManagedActivityResultLauncher<Uri, Boolean>, uri: Uri) {
+    Box(
+        modifier = Modifier
+            .height(320.dp)
+            .fillMaxWidth()
+            .clickable {
+                launcher.launch(uri)
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_baseline_add_a_photo_24),
+            contentDescription = "My Picture",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(42.dp)
+        )
+    }
+}
+
+@Composable
+fun photoView(uriRemember:String){
+    Box(){
         Image(
             painter = rememberAsyncImagePainter(
                 ImageRequest
@@ -46,9 +84,21 @@ fun AddScreen(viewModel: AddViewModel) {
                     .build()
             ),
             contentDescription = "My Picture",
-            modifier = Modifier.fillMaxSize()
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(320.dp)
+        )
+
+        Text(
+            text = "This is a dog.",
+            color = Color.White,
+            modifier = Modifier
+                .padding(32.dp)
+                .align(alignment = Alignment.BottomStart)
         )
     }
+
 }
 
 @Preview(

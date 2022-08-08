a00cca93e75a14e2dced11ffb988c118daa594bc
harutiro <harutiro2027@outlook.jp>
2022-08-08 16:17:29 +0900

    update:身長の入力画面まで作成

--- app/src/main/java/net/harutiro/xclothes/screens/profile/Profile.kt
+++ app/src/main/java/net/harutiro/xclothes/screens/profile/Profile.kt
@@ -3,21 +3,30 @@ package net.harutiro.xclothes.screens
 import android.annotation.SuppressLint
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import net.harutiro.xclothes.R
 import net.harutiro.xclothes.ui.theme.XclothesTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ProfileScreen() {
@@ -37,13 +46,150 @@ fun ProfileScreen() {
             textInput()
             Spacer(modifier = Modifier.padding(top = 32.dp))
             radioButton()
+            Spacer(modifier = Modifier.padding(top = 32.dp))
+            ageEditText()
+            Spacer(modifier = Modifier.padding(top = 32.dp))
+            heightEditText()
 
 
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun heightEditText(){
+    Row (
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .heightIn()
+    ){
+
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(42.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+//                        .fillMaxWidth()
+
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp)
+
+        ) {
+            var text by remember { mutableStateOf("") }
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text(text = "身長") },
+                placeholder = { Text(text = "身長を入力してください")},
+                singleLine = true,
+                keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+        }
+    }
+}
+
+@Composable
+fun ageEditText(){
+    Row (
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .heightIn()
+    ){
+
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(42.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+//                        .fillMaxWidth()
+
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp)
+
+        ) {
+            var text by remember { mutableStateOf("") }
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                label = { Text(text = "年齢") },
+                placeholder = { Text(text = "年齢を入力してください")},
+                singleLine = true,
+                keyboardOptions =  KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+        }
+    }
+}
+
+@Composable
+fun ageSpinner(){
+    var expanded by remember { mutableStateOf(false) }
+    val suggestions = listOf("","Item2","Item3")
+    var selectedText by remember { mutableStateOf("") }
+
+    var textfieldSize by remember { mutableStateOf(Size.Zero)}
+
+    val icon = if (expanded)
+        Icons.Filled.ArrowDropUp //it requires androidx.compose.material:material-icons-extended
+    else
+        Icons.Filled.ArrowDropDown
+
+
+    Column() {
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = { selectedText = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    //This value is used to assign to the DropDown the same width
+                    textfieldSize = coordinates.size.toSize()
+                }
+                .clickable {
+                    expanded = !expanded
+                },
+            label = {Text("Label")},
+            trailingIcon = {
+                Icon(icon,"contentDescription",
+                    Modifier.clickable { expanded = !expanded })
+            }
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(with(LocalDensity.current){textfieldSize.width.toDp()})
+        ) {
+            suggestions.forEach { label ->
+                DropdownMenuItem(
+                    onClick = {
+                        selectedText = label
+                        expanded = false
+                              },
+                    text = { Text(text = label) }
+                )
+            }
+        }
+    }
+}
+
 @Composable
 fun radioButton() {
     Column(
@@ -156,6 +302,25 @@ fun icon(){
 }
 
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun agePreview() {
+    XclothesTheme{
+        Scaffold(
+            modifier = Modifier.heightIn()
+        ) {
+            ageEditText()
+        }
+    }
+}
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)

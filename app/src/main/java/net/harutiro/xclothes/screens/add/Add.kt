b166f72280fb6f21047b7904bdba37fd7987fa82
harutiro <harutiro2027@outlook.jp>
2022-08-09 01:30:14 +0900

    update:写真が取れるようになった

--- app/src/main/java/net/harutiro/xclothes/screens/add/Add.kt
+++ app/src/main/java/net/harutiro/xclothes/screens/add/Add.kt
@@ -1,14 +1,21 @@
 package net.harutiro.test_bottomnavigation_withjetpackcompose.screens
 
 import android.content.res.Configuration
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import net.harutiro.xclothes.screens.ProfileScreen
 import net.harutiro.xclothes.screens.add.AddViewModel
 import net.harutiro.xclothes.ui.theme.XclothesTheme
@@ -18,16 +25,29 @@ fun AddScreen(viewModel: AddViewModel) {
 
     val context = LocalContext.current
 
+    var uriRemember by remember { mutableStateOf("") }
+    val uri = viewModel.photoStartUp(context)
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) { _ ->
+        uriRemember = uri.toString()
+    }
+
     Surface (
         modifier = Modifier
             ){
-        Button(
-            onClick = {
-                viewModel.putToastHello(context)
-            }
-        ) {
+        Button(onClick = { launcher.launch(uri) }) {
 
         }
+
+        Image(
+            painter = rememberAsyncImagePainter(
+                ImageRequest
+                    .Builder(LocalContext.current)
+                    .data(data = uriRemember)
+                    .build()
+            ),
+            contentDescription = "My Picture",
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
 
@@ -39,10 +59,9 @@ fun AddScreen(viewModel: AddViewModel) {
 )
 @Preview(showBackground = true, widthDp = 320)
 @Composable
-fun ProfileScreenPreview() {
-    val viewModel :AddViewModel = viewModel()
+fun AddScreenPreview() {
 
     XclothesTheme {
-        AddScreen(viewModel)
+        AddScreen(AddViewModel())
     }
 }

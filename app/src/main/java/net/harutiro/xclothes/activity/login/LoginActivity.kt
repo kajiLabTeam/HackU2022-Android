1dc774d3d9c2e5a52edccf352a573d0752658e32
harutiro <harutiro2027@outlook.jp>
2022-08-20 15:09:03 +0900

    update:ログイン部分作成

--- app/src/main/java/net/harutiro/xclothes/activity/login/LoginActivity.kt
+++ app/src/main/java/net/harutiro/xclothes/activity/login/LoginActivity.kt
@@ -1,20 +1,88 @@
 package net.harutiro.xclothes.activity.login
 
+import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import net.harutiro.xclothes.ui.theme.XclothesTheme
 
 class LoginActivity : ComponentActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    val TAG = "AuthDebug"
+
+    private lateinit var oneTapClient: SignInClient
+
+    val default_web_client_id_auth = "930686882818-acl9s28jpbmmcuvumuv6nrol9ifqfg17.apps.googleusercontent.com"
+
+    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        oneTapClient = Identity.getSignInClient(this)
+
+        Log.d(TAG, auth.currentUser?.photoUrl.toString())
+
+
+        val signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(default_web_client_id_auth)
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(false)
+                    .build())
+            .build();
+
+
+
+        oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener(this) { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null, 0, 0, 0, null)
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e("Auth2", "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(this) { e ->
+                // No saved credentials found. Launch the One Tap sign-up flow, or
+                // do nothing and continue presenting the signed-out UI.
+                Log.d("Auth3", e.localizedMessage)
+            }
+
+
+
+
         setContent {
             XclothesTheme {
                 // A surface container using the 'background' color from the theme
@@ -23,8 +91,86 @@ class LoginActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     LoginScreen()
+
+                    AsyncImage(
+                        contentDescription = "My Picture",
+                        contentScale = ContentScale.Fit,
+                        model = auth.currentUser?.photoUrl,
+                        modifier = Modifier
+                            .size(32.dp)
+                    )                }
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQ_ONE_TAP -> {
+                try {
+                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken = credential.googleIdToken
+                    when {
+                        idToken != null -> {
+                            // Got an ID token from Google. Use it to authenticate
+                            // with Firebase.
+                            Log.d(TAG, "Got ID token.")
+
+                            firebaseCertification(data)
+                        }
+                        else -> {
+                            // Shouldn't happen.
+                            Log.d(TAG, "No ID token!")
+                        }
+                    }
+                } catch (e: ApiException) {
+
+                    Log.d(TAG,e.toString())
+                    // ...
                 }
             }
+        }
+    }
+
+    fun firebaseCertification ( data : Intent?){
+        val googleCredential = oneTapClient.getSignInCredentialFromIntent(data)
+        val idToken = googleCredential.googleIdToken
+        when {
+            idToken != null -> {
+                // Got an ID token from Google. Use it to authenticate
+                // with Firebase.
+                val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+                auth.signInWithCredential(firebaseCredential)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success")
+                            val user = auth.currentUser
+
+                            Log.d(TAG, user?.email.toString())
+//                                updateUI(user)
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.exception)
+//                                updateUI(null)
+                        }
+                    }
+            }
+            else -> {
+                // Shouldn't happen.
+                Log.d(TAG, "No ID token!")
+            }
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+//            reload();
         }
     }
 }
--- app/src/main/java/net/harutiro/xclothes/activity/login/LoginScreen.kt
+++ app/src/main/java/net/harutiro/xclothes/activity/login/LoginScreen.kt
@@ -1,8 +1,17 @@
 package net.harutiro.xclothes.activity.login
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.AsyncImage
 
 @Composable
 fun LoginScreen(){
+}
+
+
+@Composable
+fun AccountIcon(url: String){
 
 }

53c3d48cc6e8a6bc76f6a66d5af90ae975185787
harutiro <harutiro2027@outlook.jp>
2022-08-22 07:41:33 +0900

    add:Apiの動作を定義

---
+++ app/src/main/java/net/harutiro/xclothes/models/coordinate/ApiCoordinateMethod.kt
@@ -0,0 +1,88 @@
+package net.harutiro.xclothes.models.coordinate
+
+import android.util.Log
+import com.google.gson.Gson
+import net.harutiro.xclothes.models.coordinate.get.GetCoordinateResponse
+import net.harutiro.xclothes.models.coordinate.post.PostCoordinateRequestBody
+import net.harutiro.xclothes.models.coordinate.post.PostCoordinateResponse
+import net.harutiro.xclothes.models.login.get.GetLoginResponse
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
+
+class ApiCoordinateMethod {
+
+    val CONNECTION_TIMEOUT_MILLISECONDS = 1000
+    val READ_TIMEOUT_MILLISECONDS = 1000
+
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(CONNECTION_TIMEOUT_MILLISECONDS.toLong(), java.util.concurrent.TimeUnit.MILLISECONDS)
+        .readTimeout(READ_TIMEOUT_MILLISECONDS.toLong(), java.util.concurrent.TimeUnit.MILLISECONDS)
+        .build()
+
+    fun coordinateGet(ble:String , nextStepFunc:() -> Unit){
+        val request = Request.Builder()
+            .url("http://20.168.98.13:8080/cordinate:?ble=$ble")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                // Responseの読み出し
+                val responseBody = response.body?.string().orEmpty()
+
+                Log.d("App", responseBody)
+
+                val gson = Gson()
+                val coordinateDate = gson.fromJson(responseBody, GetCoordinateResponse::class.java)
+
+                Log.d("App", coordinateDate.toString())
+                // 必要に応じてCallback
+
+                nextStepFunc()
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("Error", e.toString())
+                // 必要に応じてCallback
+            }
+        })
+    }
+
+    fun coordinatePost(postCoordinateRequestBody: PostCoordinateRequestBody,nextStepFunc: () -> Unit){
+
+        val gson = Gson()
+
+        val jsonData = gson.toJson(postCoordinateRequestBody)
+
+        val JSON_MEDIA = "application/json; charset=utf-8".toMediaType()
+
+
+        val request = Request.Builder()
+            .url("http://20.168.98.13:8080/cordinate")
+            .post(jsonData.toRequestBody(JSON_MEDIA))
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                // Responseの読み出し
+                val responseBody = response.body?.string().orEmpty()
+
+                Log.d("App", responseBody)
+
+                val gson = Gson()
+                val apiResponseStatus = gson.fromJson(responseBody, PostCoordinateResponse::class.java)
+
+                Log.d("App", apiResponseStatus.toString())
+                // 必要に応じてCallback
+
+                nextStepFunc()
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("Error", e.toString())
+                // 必要に応じてCallback
+            }
+        })
+    }
+}

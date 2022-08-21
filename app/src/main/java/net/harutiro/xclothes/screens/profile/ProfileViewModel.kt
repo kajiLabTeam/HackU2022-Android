3eaf476659ab6e2a82d4fdbeb653c46dc8726124
harutiro <harutiro2027@outlook.jp>
2022-08-21 21:27:30 +0900

    update:User情報を端末に保存できるようにした

--- app/src/main/java/net/harutiro/xclothes/screens/profile/ProfileViewModel.kt
+++ app/src/main/java/net/harutiro/xclothes/screens/profile/ProfileViewModel.kt
@@ -3,9 +3,13 @@ package net.harutiro.xclothes.screens.profile
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.lifecycle.AndroidViewModel
+import com.google.gson.Gson
 import net.harutiro.xclothes.models.login.ApiLoginMethod
 import net.harutiro.xclothes.models.login.post.PostLoginRequestBody
+
 
 class ProfileViewModel(application: Application): AndroidViewModel(application) {
     @SuppressLint("StaticFieldLeak")
@@ -17,13 +21,22 @@ class ProfileViewModel(application: Application): AndroidViewModel(application) 
 
     val apiLoginMethod = ApiLoginMethod()
 
+    var data: SharedPreferences = application.getSharedPreferences("DataSave", Context.MODE_PRIVATE)
+    var editor = data.edit()
+
     fun loginPost(activity: Activity){
         if(isNewProfile){
             apiLoginMethod.loginPost(userDataClass){
                 activity.finish()
             }
         }else{
+
         }
+
+        val gson = Gson()
+        val json = gson.toJson(userDataClass)
+        editor.putString(json,"")
+        editor.apply()
     }
 
 

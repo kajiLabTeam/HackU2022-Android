8de9e39d10e16cd7a0d347114f01cdf96e5f5493
harutiro <harutiro2027@outlook.jp>
2022-08-23 17:58:11 +0900

    update:クラウディナリを使えるクラスを作成

---
+++ app/src/main/java/net/harutiro/xclothes/models/coordinate/CloudinaryPost.kt
@@ -0,0 +1,57 @@
+package net.harutiro.xclothes.models.coordinate
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import coil.load
+import com.cloudinary.android.MediaManager
+import com.cloudinary.android.callback.ErrorInfo
+import com.cloudinary.android.callback.UploadCallback
+import net.harutiro.xclothes.BuildConfig
+import java.net.URI
+
+class CloudinaryPost {
+
+    private val TAG = "Cloudinary"
+
+    val config = mapOf(
+        "cloud_name" to BuildConfig.CLOUD_NAME,
+        "api_key" to BuildConfig.API_KEY,
+        "api_secret" to BuildConfig.API_SECRET
+    )
+
+    fun postPhoto(context: Context, postUri: Uri, getUrl:(String) -> Unit){
+        MediaManager.init(context, config);
+
+        MediaManager.get().upload(postUri).callback(object : UploadCallback {
+            override fun onStart(requestId: String) {
+                Log.d(TAG, "onStart: " + "started")
+            }
+
+            override fun onProgress(requestId: String, bytes: Long, totalBytes: Long) {
+                Log.d(TAG, "onStart: " + "uploading")
+            }
+
+            override fun onSuccess(requestId: String, resultData: Map<*, *>?) {
+                Log.d(TAG, "onStart: " + "usuccess" + resultData?.get("secure_url"))
+                val url = resultData?.get("secure_url").toString()
+
+                getUrl(url)
+            }
+
+            override fun onError(requestId: String?, error: ErrorInfo) {
+                Log.d(TAG, "onStart: $error")
+            }
+
+            override fun onReschedule(requestId: String?, error: ErrorInfo) {
+                Log.d(TAG, "onStart: $error")
+            }
+        }).dispatch()
+    }
+
+
+
+
+}

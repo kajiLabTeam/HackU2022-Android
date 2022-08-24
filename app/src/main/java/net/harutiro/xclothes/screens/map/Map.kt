ca8342e1d4a1f75482ebe4d954cc1648fa548c9d
harutiro <harutiro2027@outlook.jp>
2022-08-25 04:28:44 +0900

    update:mapでのマップを表示した

--- app/src/main/java/net/harutiro/xclothes/screens/map/Map.kt
+++ app/src/main/java/net/harutiro/xclothes/screens/map/Map.kt
@@ -2,7 +2,11 @@ package net.harutiro.test_bottomnavigation_withjetpackcompose.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -10,21 +14,40 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MapScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFF876ED3)),
-        contentAlignment = Alignment.Center
+
+    Scaffold(
+        modifier = Modifier.padding(0.dp,0.dp,0.dp,70.dp),
+    ){
+        Map2(it)
+    }
+}
+
+@Composable
+fun Map2(paddingValues: PaddingValues) {
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize().padding(paddingValues),
+        cameraPositionState = cameraPositionState
     ) {
-        Text(
-            text = "Map",
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
+        Marker(
+            state = MarkerState(position = singapore),
+            title = "Singapore",
+            snippet = "Marker in Singapore"
         )
     }
 }
